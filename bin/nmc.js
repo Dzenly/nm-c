c7beda044970611e623a0d21698d8e2ab462a4dd
Dzenly <alexchem@yandex.ru>
2019-06-06 17:52:40 +0700

    fix

--- bin/nmc.js
+++ bin/nmc.js
@@ -1,3 +1,7 @@
-'use strict';
+#!/usr/bin/env node
+
+// ':' //# comment; exec /usr/bin/env node "$0" "$@"
+// Don't allow eslint to set semicolon after ':' above.
+// http://sambal.org/2014/02/passing-options-node-shebang-line/
 
 console.log('Hello world');
